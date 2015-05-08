c49f860354b725e8bb3572303198ea08582bdc00
Velitheda <jasmine@revert.io>
2015-05-08 14:14:56 +1200

    iteration E3

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
     session[:counter] = 0
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
